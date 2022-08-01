2613ee841c53bcae8324ea4558507863f55326a6
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-08-01 15:49:05 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="header__container page__container">
-                <a href='/src/index.js'>
-                    <img className="header__logo-img" src={logo} alt="Logo" />
-                </a>
+                <img className="header__logo-img" src={logo} alt="Logo" />
                 <div className="header__signUp-conainer">
                     <Button classModificator="button--space-between">
                         <a href="#users" className="button__link">
