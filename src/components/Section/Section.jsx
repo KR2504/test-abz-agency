3ac6951dd58aceb1456559499168645adbe2721a
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-08-01 15:35:32 +0300

    fix json

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
-export default function Section({title, children, classModificator =''}) {
-    return (
-        <section className={`${classModificator}`}>
-            {title && <h1>{title}</h1>}
-            {children}
-        </section>
-    );
-}
+// import React from "react";
+// import PropTypes from "prop-types";
+// export default function Section({title, children, classModificator =''}) {
+//     return (
+//         <section className={`${classModificator}`}>
+//             {title && <h1>{title}</h1>}
+//             {children}
+//         </section>
+//     );
+// }
 
-Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-};
+// Section.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     children: PropTypes.node.isRequired,
+// };
